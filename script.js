f1ad3ca3f7c82cdff380711ad39833e17a56c3bd
Sergey Konstantinov <sergey.konstantinov@raiffeisen.ru>
2022-06-23 19:38:31 +0300

    base render

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ const itemTemplate = document.querySelector(".item_template").content;
 const list = document.querySelector(".list");
 const formButton = document.querySelector(".form__submit");
 const formInput = document.querySelector(".form__input");
+
+const renderItems = () => {
+	items.forEach(renderItem);
+};
+
+const renderItem = (text) => {
+	const htmlElement = itemTemplate.cloneNode(true);
+	htmlElement.querySelector('.item__text').textContent = text;
+
+	list.append(htmlElement)
+}
+
+const handleSubmit = () => {
+	renderItem(formInput.value);
+}
+
+
+formButton.addEventListener('click', handleSubmit)
+
+renderItems();
